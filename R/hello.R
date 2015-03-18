366cb82808ee62c98ed2aad8f8d5a3e6df208b36
Allen Hammock <ahammock@mac.com>
2015-03-17 20:02:41 -0700

    completed documentation for initial two test functions hello() and add()

--- R/hello.R
+++ R/hello.R
@@ -1,5 +1,26 @@
-# Hello, world!
-#
-hello <- function() {
-  print("Hello, world!")
+#' Add together two numbers.
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add(1, 1)
+#' @export
+add <- function(x, y) {
+  x + y
+}
+
+#' Say someone's name.
+#' 
+#' This is an example function that prints 'Hello, <who>!' or
+#' 'Hello, Anonymous Person!' if no argument is given.
+#' 
+#' @param who A person's name.
+#' @return A text string containing the \code{who}'s name.
+#' @examples
+#' hello("Charlie")
+#' hello()
+#' @export
+hello <- function(who = "Anonymous Person") {
+  print(paste0("Hello, ", who, "!"))
 }
